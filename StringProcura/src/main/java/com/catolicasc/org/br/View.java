3c406e95b73a1ad6b09a3edc989a5746391fb622
dayanOrlando <dayan@gatilabs.com.br>
2019-05-02 18:57:49 -0300

    Tela funcionando novo metodo construtor

--- StringProcura/src/main/java/com/catolicasc/org/br/View.java
+++ StringProcura/src/main/java/com/catolicasc/org/br/View.java
@@ -2,12 +2,24 @@ package com.catolicasc.org.br;
 
 import java.awt.EventQueue;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class View {
 
-	private JFrame frame;
-
+	private JFrame frmView;
+	private JLabel jLTexto;
+	private JTextField jTextField;
+	private JLabel jLBusca;
+	private JTextField jBuscaField;
+	private JButton jButtonBuscar;
+	private JLabel lblResposta;
 	/**
 	 * Launch the application.
 	 */
@@ -16,7 +28,7 @@ public class View {
 			public void run() {
 				try {
 					View window = new View();
-					window.frame.setVisible(true);
+					window.frmView.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -35,9 +47,46 @@ public class View {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frmView = new JFrame();
+		frmView.setTitle("View1");
+		frmView.setBounds(100, 100, 450, 300);
+		frmView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmView.getContentPane().setLayout(null);
+		
+		jLTexto = new JLabel("Digite o Texto:");
+		jLTexto.setBounds(40, 40, 100, 20);
+		frmView.getContentPane().add(jLTexto);
+		
+		jTextField = new JTextField();
+		jTextField.setBounds(40,70,100,20);
+		frmView.getContentPane().add(jTextField);
+		
+		jLBusca = new JLabel("Digite o Texto de busca:");
+		jLBusca.setBounds(40, 110, 122, 20);
+		frmView.getContentPane().add(jLBusca);
+		
+		jBuscaField = new JTextField();
+		jBuscaField.setBounds(40,150,100,20);
+		frmView.getContentPane().add(jBuscaField);
 
+		jButtonBuscar = new JButton("Buscar Palavra");
+		jButtonBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				String texto = jTextField.getText();
+				String busca = jBuscaField.getText();
+				
+				Kmp b = new Kmp(busca,texto,true);
+				lblResposta.setText(b.getMsg());
+			}
+		});
+		
+		jButtonBuscar.setBounds(40, 180, 150, 25);
+		frmView.getContentPane().add(jButtonBuscar);
+		
+		lblResposta = new JLabel("Resposta");
+		lblResposta.setBounds(40, 220,200,20);
+		frmView.getContentPane().add(lblResposta);
+
+
+	}
 }
